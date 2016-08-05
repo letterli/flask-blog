f5b33aef364bfb7efc2fcb69591ec76d604af536
letter_li <2339307889@qq.com>
2016-08-05 18:26:25 +0800

    auth

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,16 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 db = SQLAlchemy()
 moment = Moment()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -21,9 +25,13 @@ def create_app(config_name):
     mail.init_app(app)
     db.init_app(app)
     moment.init_app(app)
+    login_manager.init_app(app)
 
     # 注册蓝本
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
